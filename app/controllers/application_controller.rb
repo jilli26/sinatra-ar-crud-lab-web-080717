a4255026dfc78361674c2b9de1a639f59883c0b4
github username <github email address>
2017-08-31 15:13:26 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,53 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  #create
+  get '/posts/new' do
+    erb :new
   end
+
+  #create
+  post '/posts' do
+    @post = Post.create(params)
+    erb :posts
+  end
+
+  #read - index
+  get '/posts' do
+    @posts = Post.all
+    erb :index
+  end
+
+  #read - show (find by id)
+  get '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    erb :show
+  end
+
+  #update - render update form
+  get '/posts/:id/edit' do
+    @post = Post.find_by(id: params[:id])
+    erb :edit
+  end
+
+  #update - patch/catch post request
+  patch '/posts/:id' do
+    @post = Post.find_by(id: params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+
+    redirect to "/posts/#{@post.id}"
+
+    erb :show
+  end
+
+  #delete - delete button is a form
+  delete '/posts/:id/delete' do
+    @post = Post.find_by(id: params[:id])
+    @post.destroy
+
+    erb :deleted
+  end
+
 end
--- app/models/model.rb
+++ app/models/model.rb
@@ -1,1 +1,15 @@
-#Placeholder for a model
+class Post < ActiveRecord::Base
+
+  # @@all = []
+
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   @@all << self
+  # end
+  #
+  # def self.all
+  #   @@all
+  # end
+
+end
